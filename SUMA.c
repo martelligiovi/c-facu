e0effa7676d716b138ba2913b902ff9793c49bed
martelligiovi <martelligiovi@gmail.com>
2022-08-17 19:02:35 -0300

    first commit

---
+++ SUMA.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+  int a;
+  int b;
+  int resultado;
+  printf("ingrese numero a\n");
+  scanf("%d",&a);
+  printf("ingrese numero b\n");
+  scanf("%d",&b);
+  resultado = a + b;
+  printf("el resultado es:%d y me encanta uwu \n",resultado);
+  return 0;
+}
